f8b1c69b00dfb923eb20f6caaee820de836db62b
Moses Samuel <mosamuel98@gmail.com>
2019-05-11 22:41:52 -0700

    add two-player support to model and view

--- elm-counter/src/Main.elm
+++ elm-counter/src/Main.elm
@@ -10,13 +10,23 @@ import Html.Events exposing (onClick)
 ---- MODEL ----
 
 
+type alias Player =
+    { name : String, score : Int }
+
+
 type alias Model =
-    Int
+    { p1 : Player
+    , p2 : Player
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( { p1 = Player "Moses" 0
+      , p2 = Player "Casper" 0
+      }
+    , Cmd.none
+    )
 
 
 
@@ -24,22 +34,34 @@ init =
 
 
 type Msg
-    = Incrememnt
-    | Decrement
-    | Reset
+    = Incrememnt Player
+    | Decrement Player
+    | Reset Player
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Incrememnt ->
-            ( model + 1, Cmd.none )
+        Incrememnt player ->
+            if player.name == "Moses" then
+                ( { model | p1 = { player | score = player.score + 1 } }, Cmd.none )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+            else
+                ( { model | p2 = { player | score = player.score + 1 } }, Cmd.none )
 
-        Reset ->
-            ( 0, Cmd.none )
+        Decrement player ->
+            if player.name == "Moses" then
+                ( { model | p1 = { player | score = player.score - 1 } }, Cmd.none )
+
+            else
+                ( { model | p2 = { player | score = player.score - 1 } }, Cmd.none )
+
+        Reset player ->
+            if player.name == "Moses" then
+                ( { model | p1 = { player | score = 0 } }, Cmd.none )
+
+            else
+                ( { model | p2 = { player | score = 0 } }, Cmd.none )
 
 
 
@@ -47,12 +69,22 @@ update msg model =
 
 
 view : Model -> Html Msg
-view model =
+view { p1, p2 } =
     div []
-        [ div [] [ p [] [ text (String.fromInt model) ] ]
-        , button [ onClick Incrememnt ] [ text "+" ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , button [ onClick Reset ] [ text "reset" ]
+        [ div []
+            [ p [] [ text p1.name ]
+            , p [] [ text (String.fromInt p1.score) ]
+            ]
+        , button [ onClick (Incrememnt p1) ] [ text "+" ]
+        , button [ onClick (Decrement p1) ] [ text "-" ]
+        , button [ onClick (Reset p1) ] [ text "reset" ]
+        , div []
+            [ p [] [ text "Casper" ]
+            , p [] [ text (String.fromInt p2.score) ]
+            ]
+        , button [ onClick (Incrememnt p2) ] [ text "+" ]
+        , button [ onClick (Decrement p2) ] [ text "-" ]
+        , button [ onClick (Reset p2) ] [ text "reset" ]
         ]
 
 
