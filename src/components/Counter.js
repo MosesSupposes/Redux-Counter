fabd175da5a1cad070d41a0994930d9c5330d19c
Moses Samuel <mosamuel98@gmail.com>
2019-05-11 23:14:50 -0700

    refacotr args passed to default export

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { increment, decrement } from '../actions';
+import { increment, decrement, reset } from '../actions';
 
 class Counter extends Component {
     incrementIfOdd = () => {
@@ -45,15 +45,23 @@ class Counter extends Component {
 // this component receives the whole state. In a more complex
 // redux application, though, it would receive only the relevant
 // parts it needs from the state object.
-const mapStateToProps = (state) => {
-    return {
+const mapStateToProps = (state) => 
+    ({
         count: state.count
-    };
-};
+    });
+
+const mapDispatchToProps = (_) => 
+    ({
+        increment,
+        decrement,
+        reset,
+    })
 
 // The connect function is called in order to make this component aware
 // of the rest of the redux architecture. Without this, this component
 // is only a dumb React component. We pass in all of the functions that
 // are reliant on Redux, along with the component itself, so that Redux
 // makes itself known to this component.
-export default connect(mapStateToProps, { increment, decrement })(Counter);
+
+// export default connect(mapStateToProps, { increment, decrement, reset })(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
